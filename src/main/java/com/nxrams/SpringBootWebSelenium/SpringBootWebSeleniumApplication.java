6c73c922db0ecba7c0cca565441aba1681a2fd1a
Unknown <Unknown>
2021-11-06 10:40:52 +0530

    Second commit

---
+++ src/main/java/com/nxrams/SpringBootWebSelenium/SpringBootWebSeleniumApplication.java
@@ -0,0 +1,13 @@
+package com.nxrams.SpringBootWebSelenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSeleniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSeleniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nxrams/SpringBootWebSelenium/SpringBootWebSeleniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nxrams.SpringBootWebSelenium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebSeleniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
